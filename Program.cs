51aa3419dcadeb0786a614e0ce6db11dde361aed
TEACHER <TEACHER@K-405-5>
2024-01-12 21:00:10 +0700

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading;
+
+namespace Potoki
+{
+    internal class Program
+    {
+        private static void Method()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+                Thread.Sleep(1000);
+            }        
+        }
+        static void Main(string[] args)
+        {
+            Thread thread = new(Method);
+            thread.Start();
+            Console.WriteLine("end");
+            thread.Join();
+            Console.WriteLine("ready");
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
